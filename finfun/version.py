5930808ee6f94d882cfcd399e548678d4e5a2887
Henk van den Berg <henkvandenberg8@gmail.com>
2019-02-10 20:22:35 +0100

    doc strings

--- finfun/version.py
+++ finfun/version.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Module that keeps track of library management like version control, release dates etc.
-"""
+""" Module that keeps track of library management like version control, release dates etc. """
 
-"""
-The version number of this release.
-"""
 __version__ = '0.0.2'
 __release_date__ = '2019-02-10'
