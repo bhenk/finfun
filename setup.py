5e82677c019ba55314035fe5405a3de4d55be605
Henk van den Berg <henkvandenberg8@gmail.com>
2019-02-10 16:21:58 +0100

    firts commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='finfun',
+    version='0.0.1',
+    packages=[],
+    url='https://github.com/bhenk/finfun',
+    license='Apache License Version 2.0',
+    author='hvdb',
+    author_email='',
+    description='A collection of utilities to do financial calculations',
+    install_requires=['pandas', 'numpy']
+)
