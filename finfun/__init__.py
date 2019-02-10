b36e17f74940568461ddddcb7d1c149fd8a31184
Henk van den Berg <henkvandenberg8@gmail.com>
2019-02-10 18:50:00 +0100

    first version

--- finfun/__init__.py
+++ finfun/__init__.py
@@ -1,2 +1,3 @@
 import finfun.version
 __version__ = version.__version__
+__release_date__ = version.__release_date__
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
+import finfun.version as version
 
 setup(
     name='finfun',
-    version='0.0.1',
+    version=version.__version__,
     packages=['finfun'],
     url='https://github.com/bhenk/finfun',
     license='Apache License Version 2.0',
