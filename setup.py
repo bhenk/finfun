b6b01e08ff02d5f262fb6a7c67eab8f00e5003f2
Henk van den Berg <henkvandenberg8@gmail.com>
2019-02-10 17:01:05 +0100

    first version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='finfun',
     version='0.0.1',
-    packages=['finfun'],
+    packages=['finfun', 'finfun.finfun'],
     url='https://github.com/bhenk/finfun',
     license='Apache License Version 2.0',
     author='hvdb',
