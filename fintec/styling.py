6d0abce5cdbf7f0df319de590e1ec8dcac7ba527
Henk van den Berg <henkvandenberg8@gmail.com>
2019-03-04 20:25:37 +0100

    logging

--- fintec/styling.py
+++ fintec/styling.py
@@ -11,10 +11,11 @@ import pandas as pd
 import logging, sys
 
 __all__ = ['color_negative_red', 'c_format', 'p_format', 'currency', 'percentage',
-           'start_logging', 'end_logging', 'initiate_file_logging', 'debug', 'info']
+           'start_logging', 'end_logging', 'start_file_logging', 'end_file_logging', 'debug', 'info']
 
 _log = logging.getLogger(__name__)
-__LOG_CHANNEL__ = logging.StreamHandler(sys.stdout)
+__STDOUT_LOG_CHANNEL__ = None
+__FILE_LOG_CHANNEL__ = None
 
 
 def start_logging(level=logging.DEBUG):
@@ -23,12 +24,15 @@ def start_logging(level=logging.DEBUG):
     :param level: log level
     :return: None
     """
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s')
-    __LOG_CHANNEL__.setFormatter(formatter)
-    __LOG_CHANNEL__.setLevel(level)
-    root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
-    root.addHandler(__LOG_CHANNEL__)
+    global __STDOUT_LOG_CHANNEL__
+    if __STDOUT_LOG_CHANNEL__ is None:
+        __STDOUT_LOG_CHANNEL__ = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s')
+        __STDOUT_LOG_CHANNEL__.setFormatter(formatter)
+        __STDOUT_LOG_CHANNEL__.setLevel(level)
+        root = logging.getLogger()
+        root.setLevel(logging.DEBUG)
+        root.addHandler(__STDOUT_LOG_CHANNEL__)
 
 
 def end_logging():
@@ -36,8 +40,11 @@ def end_logging():
     Stop logging log messages to stdout.
     :return: None
     """
-    root = logging.getLogger()
-    root.removeHandler(__LOG_CHANNEL__)
+    global __STDOUT_LOG_CHANNEL__
+    if __STDOUT_LOG_CHANNEL__ is not None:
+        root = logging.getLogger()
+        root.removeHandler(__STDOUT_LOG_CHANNEL__)
+        __STDOUT_LOG_CHANNEL__ = None
 
 
 def debug(func, *args, **kwargs):
@@ -73,6 +80,7 @@ def info(func, *args, **kwargs):
 
 
 class CsvFormatter(logging.Formatter):
+
     def __init__(self):
         super().__init__()
         self.output = StringIO()
@@ -88,13 +96,13 @@ class CsvFormatter(logging.Formatter):
         return data.strip()
 
 
-def initiate_file_logging(log_file='logs/fintec.log', level=logging.DEBUG, max_bytes=1000 * 1000 * 1024,
+def start_file_logging(log_file='logs/pyu.log', level=logging.DEBUG, max_bytes=1000 * 1000 * 1024,
                           backup_count=3, encoding='utf-8'):
     """
     Initiate logging to a rotating file. If needed, the log file output can be picked up in a DataFrame:
     ```
     names = ['date', 'thread', 'process', 'level', 'file', 'line', 'function', 'msg', 'path']
-    df = pd.read_csv('logs/fintec.log', header=None, quoting=1, converters={0: pd.to_datetime}, names=names)
+    df = pd.read_csv('logs/pyu.log', header=None, quoting=1, converters={0: pd.to_datetime}, names=names)
     ```
 
     :param log_file: the path or file to write to. Directories will be created.
@@ -105,15 +113,35 @@ def initiate_file_logging(log_file='logs/fintec.log', level=logging.DEBUG, max_b
     :param encoding: encoding of the file
     :return: None
     """
-    path = os.path.dirname(log_file)
-    os.makedirs(path, exist_ok=True)
-    log_channel = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count, encoding=encoding)
-    log_channel.setFormatter(CsvFormatter())
-    log_channel.setLevel(level)
-    root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
-    root.addHandler(log_channel)
-    _log.debug('Initiated file logging to {}'.format(log_file))
+    global __FILE_LOG_CHANNEL__
+    if __FILE_LOG_CHANNEL__ is None:
+        path = os.path.dirname(log_file)
+        os.makedirs(path, exist_ok=True)
+        __FILE_LOG_CHANNEL__ = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count,
+                                                   encoding=encoding)
+        __FILE_LOG_CHANNEL__.setFormatter(CsvFormatter())
+        __FILE_LOG_CHANNEL__.setLevel(level)
+        root = logging.getLogger()
+        root.setLevel(logging.DEBUG)
+        root.addHandler(__FILE_LOG_CHANNEL__)
+        _log.info('Started file logging to {}'.format(__FILE_LOG_CHANNEL__.baseFilename))
+    else:
+        _log.info('Not initiating file logging. Logging to file already established: {}'
+                     .format(__FILE_LOG_CHANNEL__.baseFilename))
+
+
+def end_file_logging():
+    """
+    End logging to a rotating file that was started with `initiate_file_logging`.
+
+    :return: None
+    """
+    global __FILE_LOG_CHANNEL__
+    if __FILE_LOG_CHANNEL__ is not None:
+        _log.info('End file logging to {}'.format(__FILE_LOG_CHANNEL__.baseFilename))
+        root = logging.getLogger()
+        root.removeHandler(__FILE_LOG_CHANNEL__)
+        __FILE_LOG_CHANNEL__ = None
 
 
 def color_negative_red(val) -> str:
